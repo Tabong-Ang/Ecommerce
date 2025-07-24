8467e1e5837c376e726a421eeb2cf0a736eb1574
Tabong-Ang <tabongphilip@gmail.com>
2025-07-24 09:12:06 +0100

    updated host url

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['ecommerce-production-d757.up.railway.app', 'https://ecommerce-production-d757.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://ecommerce-production-d757.up.railway.app']
 
 
 # Application definition
