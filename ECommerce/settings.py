244d753838476e700418d570bf3de9ad253c0176
Tabong-Ang <tabongphilip@gmail.com>
2025-07-26 08:51:27 +0100

    Added render.yaml

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['ecommerce-qifc.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://ecommerce-qifc.onrender.com']
 
 
 # Application definition
