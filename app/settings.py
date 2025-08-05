fd799177143156b133eb354177d2f8f643f5ee80
Tabong-Ang <tabongphilip@gmail.com>
2025-08-05 05:01:14 +0100

    updated db

--- app/settings.py
+++ app/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['ecommerce-production-8470.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://ecommerce-production-8470.up.railway.app']
 
 
 # Application definition
